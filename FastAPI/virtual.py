55061a924347ddcc424e20ebc2bfa95a3d5011d1
budhaparbat4u <erparbatbudha@gmail.com>
2024-01-28 16:49:20 +0545

    It contains information about api'

---
+++ FastAPI/virtual.py
@@ -0,0 +1,14 @@
+#1 cmd to create virtual environment
+
+'''
+py -3 -m venv <name of virtual environment>
+'''
+#2 cmd to run host
+"""
+uvicorn main_file:API_object
+eg:
+uvicorn main:app
+
+OR
+uvicorn main_file:API_object --reload
+"""
